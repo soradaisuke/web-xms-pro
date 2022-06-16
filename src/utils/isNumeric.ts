d0851d9555e8a1a615bc670e2aaed3e1f619d7f9
yangzheng <yangzheng@qingting.fm>
2022-06-16 16:54:10 +0800

    fix(NoTicket): x.y.z格式字符串被判断为数字类型

--- src/utils/isNumeric.ts
+++ src/utils/isNumeric.ts
@@ -2,5 +2,9 @@ export default function isNumeric(str: string): boolean {
   if (typeof str !== 'string') {
     return false;
   }
-  return !Number.isNaN(str) && !Number.isNaN(parseFloat(str));
+  return (
+    !Number.isNaN(str) &&
+    !Number.isNaN(parseFloat(str)) &&
+    String(parseFloat(str)) === str
+  );
 }
